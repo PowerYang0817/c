45c84788d38009c10c542ac8c7b4b723da43d0e8
PowerYang0817 <759541305@qq.com>
2025-01-23 19:46:23 +0800

    second save

--- test01/demo03.c
+++ test01/demo03.c
@@ -17,6 +17,12 @@ int dev(int a,int b){
 int main(int argc, char const *argv[]){
     int (*pf)(int,int) = add; // 函数指针
     printf("%d\n", pf(1, 2)); // 调用函数指针指向的函数
+    int (*p2)(int ,int) = sub;
+    printf("%d\n", p2(1, 2));
+    int (*p3)(int ,int) = mul;
+    printf("%d\n", p3(1, 2));
+    int (*p4)(int ,int) = dev;
+    printf("%d\n", p4(1, 2));
     return 0;
 
 }
